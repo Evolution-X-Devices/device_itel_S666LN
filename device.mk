b605d29b2b0c55d5d99fdd535b6be59b6c0435a1
Shirayuki39 <lorddemecrius83@proton.me>
2025-12-17 21:41:03 +0700

    S666LN: Add 1080p60 for Aperture
    Signed-off-by: KimelaZX <mmikailrei@gmail.com>

--- device.mk
+++ device.mk
@@ -333,6 +333,9 @@ PRODUCT_PACKAGES += \
     TetheringConfigTarget \
     WifiResTarget
 
+PRODUCT_PACKAGES += \
+	ApertureOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
