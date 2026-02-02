18f67d31c213743bc8aba8e49f310dd79a563aa5
Mashopy <eliasgheeraert@gmail.com>
2026-02-02 07:38:55 +0000

    S666LN: Properly patch blobs related to Stagefright
    Audio primary HAL doesn't have anything related to Stagefright, so there is no need to add
    libstagefright_foundation-v33.so dependency int it. Same goes for the C2 service as
    it's the C2 vdec/venc and plugin libs that has references for it.
    
    And let's patch the sensor HAL too just in case.
    
    There is no need to patch the system libs that depends over it as they aren't built
    over Android 13 like the vendor libs are.
    
    Signed-off-by: KimelaZX <mmikailrei@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -50,8 +50,16 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/mnld', 'vendor/lib64/mt6789/libaalservice.so', 'vendor/lib64/mt6789/libcam.utils.sensorprovider.so'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
-        .replace_needed('libalsautils.so', 'libalsautils-v31.so')
-        .add_needed('libstagefright_foundation-v33.so'),
+        .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
+    (
+        'vendor/lib64/hw/sensors.mediatek.V2.0.so',
+        'vendor/lib64/libcodec2_mtk_c2store.so',
+        'vendor/lib64/libcodec2_mtk_vdec.so',
+        'vendor/lib64/libcodec2_mtk_venc.so',
+        'vendor/lib64/libcodec2_vpp_qt_plugin.so',
+        'vendor/lib64/libcodec2_vpp_rs_plugin.so'
+    ): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint-service.trustonic': blob_fixup()
         .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
